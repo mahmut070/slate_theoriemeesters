4460ac73c69e57fe62d87be6226d420d9d4e1feb
Mahmut Gurbuz <mahmut@lokaalgevonden.nl>
2015-01-05 09:34:34 +0100

    progress

--- dev/php/templates/header.php
+++ dev/php/templates/header.php
@@ -36,6 +36,17 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.min.js"></script>
     <!-- /build -->
 
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-23859796-4', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+
     <?php if(is_page('success')) { ?>
                 <!-- Google Code for Aanmelding afgeronnd Conversion Page -->
         <script type="text/javascript">
